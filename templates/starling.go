8bacdff287e035af9ff8e7e7c95dd110a01b502c
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-05-20 18:39:13 +0100

    Fix issue with starling atlas descriptor

--- templates/starling.go
+++ templates/starling.go
@@ -1,6 +1,6 @@
 package templates
 
-const starlingStr = `<TextureAtlas imagePath="{{.ImagePath}}">
+const starlingStr = `<TextureAtlas imagePath="{{.ImageFilename}}">
 {{- range .Sprites}}
     <SubTexture name="{{.Name}}" x="{{.Left}}" y="{{.Top}}" width="{{.Width}}" height="{{.Height}}"/>
 {{- end}}
