f57db78912765651c2dfc8b3fadadebe7f27b533
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-05-28 16:17:47 +0100

    Refactor common all files test

--- packer/input_test.go
+++ packer/input_test.go
@@ -9,52 +9,17 @@ import (
 	"github.com/RaniSputnik/lovepac/packer"
 )
 
-var fixtures = map[string]struct{}{
-	"button_active.png":  {},
-	"button_hover.png":   {},
-	"button.png":         {},
-	"character_evil.png": {},
-	"character_hero.png": {},
-}
-
 func TestFileStreamSendsAllFiles(t *testing.T) {
+	var fixtures = map[string]struct{}{
+		"button_active.png":  {},
+		"button_hover.png":   {},
+		"button.png":         {},
+		"character_evil.png": {},
+		"character_hero.png": {},
+	}
+
 	assetStreamer := packer.NewFileStream("./fixtures")
-	assets, errc := assetStreamer.AssetStream(context.Background())
-	results := map[string]int{}
-	resultsChan := make(chan string, len(fixtures))
-
-	go func(results chan<- string) {
-		defer close(results)
-		for asset := range assets {
-			assetName := asset.Asset()
-			if _, ok := fixtures[assetName]; !ok {
-				t.Errorf("Found unexpected asset named '%s'", assetName)
-			} else {
-				// No select needed because we know this
-				// channel is buffered to receive the correct
-				// number of results
-				results <- assetName
-			}
-		}
-	}(resultsChan)
-
-	if err := <-errc; err != nil {
-		t.Errorf("Expected no error, got '%s'", err)
-	}
-
-	for result := range resultsChan {
-		results[result]++
-	}
-
-	for fixture := range fixtures {
-		n, ok := results[fixture]
-		if !ok {
-			t.Errorf("Expected '%s' but was never received", fixture)
-		}
-		if n > 1 {
-			t.Errorf("Expected to recieve '%s' once but was received '%d' times", fixture, n)
-		}
-	}
+	testAssetStreamerSendsAllFiles(t, assetStreamer, fixtures)
 }
 
 func TestFileStreamReportsErrorWhenDirectoryDoesNotExist(t *testing.T) {
@@ -98,9 +63,21 @@ func TestFilenameStreamSendsAllFiles(t *testing.T) {
 	}
 
 	assetStreamer := packer.NewFilenameStream("./fixtures", files...)
-	assets, errc := assetStreamer.AssetStream(context.Background())
+	testAssetStreamerSendsAllFiles(t, assetStreamer, expect)
+}
+
+func TestFilenameStreamIsCancellable(t *testing.T) {
+	assetStreamer := packer.NewFilenameStream("./fixtures", "button_active.png", "button_hover.png", "button.png")
+	testAssetStreamerIsCancellable(t, assetStreamer)
+}
+
+// Common AssetStreamer test suite //
+// ******************************* //
+
+func testAssetStreamerSendsAllFiles(t *testing.T, assetStream packer.AssetStreamer, expect map[string]struct{}) {
+	assets, errc := assetStream.AssetStream(context.Background())
 	results := map[string]int{}
-	resultsChan := make(chan string, len(fixtures))
+	resultsChan := make(chan string, len(expect))
 
 	go func(results chan<- string) {
 		defer close(results)
@@ -135,14 +112,6 @@ func TestFilenameStreamSendsAllFiles(t *testing.T) {
 		}
 	}
 }
-
-func TestFilenameStreamIsCancellable(t *testing.T) {
-	assetStreamer := packer.NewFilenameStream("./fixtures", "button_active.png", "button_hover.png", "button.png")
-	testAssetStreamerIsCancellable(t, assetStreamer)
-}
-
-// Common AssetStreamer test suite //
-// ******************************* //
 
 func testAssetStreamerIsCancellable(t *testing.T, assetStream packer.AssetStreamer) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
