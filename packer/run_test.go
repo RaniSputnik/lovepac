730b30a30d085c99a1978d47e9daf53bca7e0555
Ryan Loader <ryan.loader@oneleggedcrab.com>
2017-05-29 21:31:12 +0100

    :red_circle: Create multiple atlases when out of space

--- packer/run_test.go
+++ packer/run_test.go
@@ -117,3 +117,48 @@ func TestRunWithNilParamsResultsInError(t *testing.T) {
 		t.Errorf("Expected run with nil input to fail with error but but did not get an error")
 	}
 }
+
+func TestRunWithTooManyFilesForOneAtlasResultsInMultipleAtlases(t *testing.T) {
+	files := []string{
+		"button_active.png",
+		"button_hover.png",
+		"button.png",
+		"character_evil.png",
+		"character_hero.png",
+	}
+	expected := map[string]string{
+		fmt.Sprintf("%s-1.png", packer.DefaultAtlasName): "",
+		fmt.Sprintf("%s-1.lua", packer.DefaultAtlasName): "",
+		fmt.Sprintf("%s-2.png", packer.DefaultAtlasName): "",
+		fmt.Sprintf("%s-2.lua", packer.DefaultAtlasName): "",
+	}
+
+	outputRecorder := packer.NewOutputRecorder()
+	params := &packer.Params{
+		Input:  packer.NewFilenameStream("./fixtures", files...),
+		Output: outputRecorder,
+		// Here's the crutial part - constrain the width
+		// to a size too small for all of the files to fit
+		Width:  400,
+		Height: 400,
+	}
+
+	err := packer.Run(context.Background(), params)
+	got := outputRecorder.Got()
+
+	if err != nil {
+		t.Errorf("Expected run to succeed without error but got '%s'", err)
+	}
+
+	for gotFile := range got {
+		if _, ok := expected[gotFile]; !ok {
+			t.Errorf("Got unexpected file '%s'", gotFile)
+		}
+	}
+
+	for expect := range expected {
+		if _, ok := got[expect]; !ok {
+			t.Errorf("Expected file '%s' to be outputted", expect)
+		}
+	}
+}
