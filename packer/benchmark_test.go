dab071208bd58842523a7b4dd2975435c1e16718
Ryan Loader <rani_sputnik@hotmail.com>
2018-04-09 20:31:47 +0100

    Add benchmarks for packing a single asset

--- packer/benchmark_test.go
+++ packer/benchmark_test.go
@@ -12,27 +12,48 @@ import (
 	"github.com/RaniSputnik/lovepac/target"
 )
 
+var (
+	allAssets = "../assets"
+	oneAsset  = "../assets/button_active.png"
+)
+
 func BenchmarkPack512x512(b *testing.B) {
-	benchmarkPackX(b, 512, 512)
+	benchmarkPackX(b, 512, 512, allAssets)
 }
 
 func BenchmarkPack1024x1024(b *testing.B) {
-	benchmarkPackX(b, 1024, 1024)
+	benchmarkPackX(b, 1024, 1024, allAssets)
 }
 
 func BenchmarkPack2048x2048(b *testing.B) {
-	benchmarkPackX(b, 2048, 2048)
+	benchmarkPackX(b, 2048, 2048, allAssets)
 }
 
 func BenchmarkPack4096x4096(b *testing.B) {
-	benchmarkPackX(b, 2048, 2048)
+	benchmarkPackX(b, 2048, 2048, allAssets)
 }
 
-func benchmarkPackX(b *testing.B, w, h int) {
+func BenchmarkPackOneAsset512x512(b *testing.B) {
+	benchmarkPackX(b, 512, 512, oneAsset)
+}
+
+func BenchmarkPackOneAsset1024x1024(b *testing.B) {
+	benchmarkPackX(b, 1024, 1024, oneAsset)
+}
+
+func BenchmarkPackOneAsset2048x2048(b *testing.B) {
+	benchmarkPackX(b, 2048, 2048, oneAsset)
+}
+
+func BenchmarkPackOneAsset4096x4096(b *testing.B) {
+	benchmarkPackX(b, 2048, 2048, oneAsset)
+}
+
+func benchmarkPackX(b *testing.B, w, h int, assets string) {
 	params := &packer.Params{
 		Name:   "myatlas",
 		Format: target.Love,
-		Input:  packer.NewFileStream("../assets"),
+		Input:  packer.NewFileStream(assets),
 		Output: packer.NewFileOutputter(fmt.Sprintf("../build")),
 		Width:  w,
 		Height: h,
